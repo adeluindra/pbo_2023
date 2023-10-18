b187cc6c2216736c72f246df3b494bd633103414
Moh Syafiq Ade Luwindra <148308247+adeluindra@users.noreply.github.com>
2023-10-18 16:41:48 +0700

    Add files via upload
    menghitung luas dan keliling persegi panjang

---
+++ pertemuan1.py
@@ -0,0 +1,17 @@
+#persegi panjang
+print("menghitung luas dan keliling persegi panjang")
+
+#variable
+panjang = float(input('masukan panjang : '))
+lebar = float(input('masukan lebar : '))
+
+#rumus
+luas = panjang * lebar
+kel = 2 * (panjang + lebar)
+
+#output
+print("panjang : ", panjang)
+print("lebar : ", lebar)
+print("luas : ", luas)
+print("keliling : ", kel)
+
