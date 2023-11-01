eca7d4c772e53e410d7c0866d60e7002ca9b111d
Moh Syafiq Ade Luwindra <148308247+adeluindra@users.noreply.github.com>
2023-11-01 16:45:38 +0700

    Add files via upload

---
+++ Pertemuan1/kubus.py
@@ -0,0 +1,15 @@
+#kubus
+print('menghitung luas dan volume kubus')
+
+#variable
+sisi = float(input('masukan sisi : '))
+
+#rumus
+luas = 6 * sisi * sisi
+volume = sisi * sisi * sisi
+
+#output
+print('jumlah dari sisi : ', luas)
+print('volume dari kubus : ', volume)
+
+
